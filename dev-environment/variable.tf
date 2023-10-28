1762461f20e77fef1370b76f82c4f486c276527c
Paul <choirmaster17@yahoo.com>
2023-10-28 21:32:14 +0100

    rock-genius

--- dev-environment/variable.tf
+++ dev-environment/variable.tf
@@ -1,37 +1,111 @@
-variable "project_name" {}
-variable "region" {}
-variable "instance_tenancy" {}
-variable "vpc_cidr_block" {}
-variable "enable_dns_hostnames" {}
-variable "enable_dns_support" {}
-variable "pubsub_cidrs" {}
-variable "ptesub_cidrs" {}
-variable "internet_gateway" {}
-variable "nat_gateway" {}
-variable "nat_gateway_route_cidr" {}
-variable "inbound_ports" {}
-variable "username" {}
-variable "instance_class" {}
-variable "instance_type" {}
-variable "identifier" {}
-variable "ami" {}
-variable "engine_version" {}
-variable "allocated_storage" {}
-variable "storage_type" {}
-variable "domain_name" {}
-variable "alternative_name" {}
-variable "key_name" {}
-variable "container_name" {}
-variable "container_port" {}
-variable "load_balancer_type" {}
-variable "target_type" {}
-variable "skip_final_snapshot" {}
-variable "engine" {}
-variable "password" {}
-variable "cluster_name" {}
-variable "publicly_accessible" {}
-variable "records" {}
-variable "validation_method" {}
-variable "private_zone" {}
-variable "network_mode" {}
-variable "launch_configuration_name" {}
+variable "project_name" {
+    default = "sika"
+}
+variable "region" {
+    default = "eu-west-2"
+}
+variable "instance_tenancy" {
+    default = "default"
+}
+variable "vpc_cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "enable_dns_hostnames" {
+    default = true
+}
+variable "enable_dns_support" {
+    default = true
+}
+variable "pubsub_cidrs" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "ptesub_cidrs" {
+    default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+variable "internet_gateway" {
+    default = "sika_IGW"
+}
+variable "nat_gateway" {
+    default = "sika_nat_gateway"
+}
+variable "nat_gateway_route_cidr" {
+    default = "0.0.0.0/0"
+}
+variable "inbound_ports" {
+    default = [443]
+}
+variable "username" {
+    default = "dbsika"
+}
+variable "instance_class" {
+    default = "db.t3.micro"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "identifier" {
+    default = "database"
+}
+variable "ami" {
+    default = "ami-0b2287cff5d6be10f"
+}
+variable "engine_version" {
+    default = "14.7"
+}
+variable "allocated_storage" {
+    default = 20
+}
+variable "storage_type" {
+    default = "gp2"
+}
+variable "domain_name" {
+    default = "teamjuju.click"
+}
+variable "alternative_name" {
+    default = "*.teamjuju.click"
+}
+variable "key_name" {
+    default = "sika-key-pair"
+}
+variable "container_name" {
+    default = "sika-container-1"
+}
+variable "container_port" {
+    default = 80
+}
+variable "load_balancer_type" {
+    default = "application"
+}
+variable "target_type" {
+    default = "ip"
+}
+variable "skip_final_snapshot" {
+    default = true
+}
+variable "engine" {
+    default = "postgres"
+}
+variable "password" {
+    default = "pw052301"
+}
+variable "cluster_name" {
+    default = "sika-cluster-name" 
+}
+variable "publicly_accessible" {
+    default = true
+}
+variable "records" {
+    default = ["10.0.0.1"]
+}
+variable "validation_method" {
+    default = "DNS"
+}
+variable "private_zone" {
+    default = false
+}
+variable "network_mode" {
+    default = "awsvpc"
+}
+variable "launch_configuration_name" {
+    default = "sika-launch"
+}
